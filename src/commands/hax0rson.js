9b9c3c6369f7482610b9d72f3b8bbad53af172f9
Marcos Bérgamo <mbergamo@inviron.com.br>
2014-06-10 19:14:41 -0300

    hax0rson adicionado...

---
+++ src/commands/hax0rson.js
@@ -0,0 +1,29 @@
+﻿/*
+ * Pega um tweet aleatório do grande bot sábio do twitter @hax0rson
+ */
+
+var Twitter = require('ntwitter');
+
+var twitter = new Twitter({
+  consumer_key: 'euEYzsSOz8BGal66eUwZWoHFu',
+  consumer_secret: 'jcYUgOELNqurChfjqlMR24Wustv6uizfBgEwgHhWRAKbHWYSSm',
+  access_token_key: '53946176-woxhsqRcwCoksBrHyvej7mweJIiLPgJoEapaKuvyk',
+  access_token_secret: 'amMFIiDJEsRnBq1gkkD6tDSonZOWTy4c5JlAqLsju4'
+});
+
+var twit = function(bot, data, nick, args, end) {
+	
+	bot.message('Confie no sábio!');
+	
+	twitter.search('hax0rson', {result_type:'mixed', count:4}, function(err, data) {
+		if(err){
+			bot.message(err);
+			end();
+		}
+		bot.message(data);
+		end();
+	});
+	
+};                     
+
+exports.run = twit;
